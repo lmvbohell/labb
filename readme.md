4683c3dfc2581c61ffa8fb090e4c64a1a9be1437
Bo Hellström <bo.hellstrom@lm.se>
2020-02-19 12:40:49 +0100

    Removed some code

---
+++ readme.md
@@ -0,0 +1,3 @@
+För att kunna köra Git kommandon i VScode:
+https://code.visualstudio.com/docs/editor/integrated-terminal	-Rubrik Windows
+Ovan ex editera settings.json men // Git Bash.
--- script.py
+++ script.py
@@ -1,21 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("https://insikten.lm.se")
-print(r)
+print(r.status_code)
+print(r.ok)
 
-# print(greet('World'))
-# print(greet('Corey'))
